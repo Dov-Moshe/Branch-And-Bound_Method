e3482744715edc1ee14d8ecbd3bf28a75817c467
Dov Moshe <dovbarak.moshe@live.biu.ac.il>
2022-02-27 19:02:38 +0200

    first update

---
+++ IntegerProgrammingRegular.py
@@ -0,0 +1,23 @@
+from src.BBTree import BBTree, PSEUDOCOST_BRANCHING, MOST_FRACTIONAL, FIXED_BRANCHING, DEPTH_FIRST, BEST_FIRST, BEST_ESTIMATE
+from src.BranchAndBound import GenerateRandomMIP, BranchAndBound
+
+branch_strategy_list = [PSEUDOCOST_BRANCHING, MOST_FRACTIONAL, FIXED_BRANCHING]
+search_strategy_list = [DEPTH_FIRST, BEST_FIRST, BEST_ESTIMATE]
+
+CONSTRAINTS, VARIABLES, OBJ, MAT, RHS = GenerateRandomMIP(numVars = 10, numCons = 10, density = 0.2,
+                                                          maxObjCoeff = 10, maxConsCoeff = 10,
+                                                          rand_seed = 4)
+for i in range(len(search_strategy_list)):
+    for j in range(len(branch_strategy_list)):
+        T = BBTree()
+        T.set_display_mode('matplotlib')
+        opt, LB = BranchAndBound(T, CONSTRAINTS, VARIABLES, OBJ, MAT, RHS,
+        branch_strategy = branch_strategy_list[j],
+        search_strategy = search_strategy_list[i],
+        display_interval = 10000, binary_vars=False)
+        T.set_layout('dot')
+        f = open('search_strategy_' + search_strategy_list[i] + '_branch_strategy_' + branch_strategy_list[j] + '.png', 'wb')
+        T.write(f, format='png')
+        print('########################################################\n'
+              '########################################################\n'
+              '########################################################\n')
